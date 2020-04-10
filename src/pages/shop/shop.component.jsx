b7783e6335ad46a9d14a6cda4008636fec4e125a
David Jensen <djensen81@gmail.com>
2020-04-10 16:01:15 -0400

    added in new collection preview component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+
 import SHOP_DATA from './shop.data.js'
 
 
@@ -13,7 +15,16 @@ class ShopPage extends React.Component {
     }
 
     render() {
-        return <div>SHOP PAGE</div>
+        const {collections} = this.state;
+        return (
+            <div className='shop-page'>
+                {
+                    collections.map(({ id, ...otherCollectionProps }) => (
+                        <CollectionPreview key={id} {...otherCollectionProps} />
+                    ))
+                }
+            </div>
+        )
     }
 }
 
